34d6eb30127019620a71d37265beaf36442a2881
EC2 Default User <ec2-user@ip-172-31-23-136.eu-west-2.compute.internal>
2020-03-06 14:13:29 +0000

    added user_data si security_group

--- main.tf
+++ main.tf
@@ -5,11 +5,30 @@ provider "aws"{
 resource "aws_instance" "example" {
     ami = data.aws_ami.ubuntu.id
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+    
+    user_data = <<-EOF
+           #!/bin/bash
+           echo "Hello, World" > index.html
+           nohup busybox httpd -f -p 8080 & 
+           EOF
     
     tags = {
          Name = "CarteExample"
     }
 }  
+
+resource "aws_security_group" "instance"{
+    name = "terraform-example-instance"
+    ingress {
+        from_port  = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+   
+
 
 data "aws_ami" "ubuntu" {
 most_recent = true
